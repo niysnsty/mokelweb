94ea93ab7d0904777ba4884e5c276e13ee3cb6eb
niysnsty <aixzniaa@gmail.com>
2025-12-16 12:27:55 +0700

    memindahkan kode ke js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+document
+        .getElementById("wa-form")
+        .addEventListener("submit", function (e) {
+          e.preventDefault(); // Mencegah form submit default
+
+          const nama = document.getElementById("nama").value;
+          const email = document.getElementById("email").value;
+          const nohp = document.getElementById("nohp").value;
+
+          const pesan = `Halo, saya ingin menghubungi MnM Drink.%0A%0ANama: ${nama}%0AEmail: ${email}%0ANo HP: ${nohp}`;
+
+          // Ganti 6281234567890 dengan nomor WA tujuan (gunakan format internasional tanpa tanda +)
+          const nomorWA = "6285932125532";
+
+          const url = `https://wa.me/qr/O6HRANAY5CHJO1`;
+
+          window.open(url, "_blank"); // Buka di tab baru
+        });
